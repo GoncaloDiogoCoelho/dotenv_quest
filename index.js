a3ad987d9247c98bf686e4addaddac2a55be59e7
GonçaloDiogoCoelho <goncalodmvm.c@gmail.com>
2022-10-31 17:18:01 +0000

    last commit

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-if (process.env.NODE_ENV) {
-    require('dotenv').config({
-        path: `${__dirname}/.env.${process.env.NODE_ENV}`,
-    });
-} else {
+// if (process.env.NODE_ENV) {
+//     require('dotenv').config({
+//         path: `${__dirname}/.env.${process.env.NODE_ENV}`,
+//     });
+// } else {
     require('dotenv').config();
-}
+// }
 
 console.log(`I am ${process.env.MY_NAME}, I am from ${process.env.MY_CITY} and I love ${process.env.MY_LANGUAGE}` );
